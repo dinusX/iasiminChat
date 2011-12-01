73c224c6bfdc424eab783f876d9939d1f42614da
Schimodie <radu.gagos@gmail.com>
2011-12-01 11:54:38 +0200

    added some minor server implementation

--- ChatServer/ChatServer/Program.cs
+++ ChatServer/ChatServer/Program.cs
@@ -9,6 +9,10 @@ namespace ChatServer
     public class Program
     {
         static void Main(string[] args)
-        {}
+        {
+            Server server = new Server();
+
+            server.Run();
+        }
     }
 }
--- ChatServer/ChatServer/Server.cs
+++ ChatServer/ChatServer/Server.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace ChatServer
 {
     public class Server
     {
+        private int port;
+        private IPAddress localIP = IPAddress.Parse("127.0.0.1");
         private Random random = new Random();
-        private uint port;
+        private TcpListener server;
 
         public Server()
         {
@@ -17,14 +22,14 @@ namespace ChatServer
 
             do
             {
-                this.port = (uint) (random.Next(50000) + 10000);
+                this.port = (random.Next(50000) + 10000);
 
                 IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
                 TcpConnectionInformation[] tcpConnectionInformationArray = ipGlobalProperties.GetActiveTcpConnections();
 
                 foreach (TcpConnectionInformation tcpi in tcpConnectionInformationArray)
                 {
-                    if ((uint) tcpi.LocalEndPoint.Port == port)
+                    if (tcpi.LocalEndPoint.Port == port)
                     {
                         isAvailable = false;
 
@@ -35,6 +40,48 @@ namespace ChatServer
             while (!isAvailable);
         }
 
-        public uint Port { get { return this.port; } }
+        public void Run()
+        {
+            try
+            {
+                server = new TcpListener(this.localIP, this.port);
+
+                server.Start(1000);
+                Console.WriteLine("Server started running at {0}:{1}", this.localIP, this.port);
+
+                while (true)
+                {
+                    try 
+                    {
+                        TcpClient client = server.AcceptTcpClient();
+
+                        (new Thread(this.SatisfyClient)).Start((object)client);
+                    }
+                    catch (SocketException se) { Console.WriteLine("SocketException: {0}", se); }
+                    catch (InvalidOperationException ioe) { Console.WriteLine("InvalidOperationException: {0}", ioe); }
+                    catch (OutOfMemoryException oome) { Console.WriteLine("OutOfMemoryException: {0}", oome); }
+                    catch (ThreadStateException tse) { Console.WriteLine("ThreadStateException: {0}", tse); }
+                    catch (ArgumentNullException ane) { Console.WriteLine("ArgmunetNullException: {0}", ane); }
+                }
+            }
+            catch (InvalidOperationException ioe) { Console.WriteLine("InvalidOperationException: {0}", ioe); }
+            catch (ArgumentOutOfRangeException aoore) { Console.WriteLine("ArgumentOutOfRangeException: {0}", aoore); }
+            catch (SocketException se) { Console.WriteLine("SocketException: {0}", se); }
+            catch (ArgumentNullException ane) { Console.WriteLine("ArgmunetNullException: {0}", ane); }
+            finally 
+            {
+                try { server.Stop(); }
+                catch (SocketException se) { Console.WriteLine("SocketException: {0}", se); }
+            }
+        }
+
+        private void SatisfyClient(object cl)
+        {
+            TcpClient client = (TcpClient)cl;
+
+            Console.WriteLine("Accepted client from: {0}", client.Client.RemoteEndPoint);
+
+            client.Close();
+        }
     }
 }
